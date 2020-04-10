673cfa4f52f2c39ca2acf315540ce8c7daa7a2a4
MrFei97 <62885244+MrFei97@users.noreply.github.com>
2020-04-10 08:42:16 -0300

    Add files via upload

---
+++ TP1/Trabajo practica/libreria.c
@@ -0,0 +1,62 @@
+#include "libreria.h"
+
+
+float sumaDeNumeros(float num1, float num2){
+
+float total;
+
+total = num1 + num2;
+
+return(total);
+}
+
+
+float resta(float num1, float num2){
+
+float result;
+
+result = num1 - num2;
+
+return(result);
+
+}
+
+
+float multiplicar(float num1, float num2){
+
+    float result;
+
+result = num1 * num2;
+
+return(result);
+
+}
+
+
+float dividir(float num1, float num2){
+
+float result;
+
+result = num1 / num2;
+
+
+return(result);
+
+
+}
+
+
+float factorial(float num) {
+
+   float result;
+
+    if(num==1){
+        return 1;
+    }
+    else {
+    result=num*factorial(num-1);
+
+    return(result);
+
+}
+}
